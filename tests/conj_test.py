86ab2f2c4b1c5a9a31ae10e5576c62a98192c117
pboisver <pboisver@umich.edu>
2024-02-08 16:48:47 -0500

    Ready to test

--- tests/conj_test.py
+++ tests/conj_test.py
@@ -1,57 +1,135 @@
 from loguru import logger
-from rdflib import RDF, Graph, Literal, URIRef
-from rdflib import FOAF
-from rdflib.namespace import NamespaceManager
+from rdflib import FOAF, RDF, ConjunctiveGraph, Graph, Literal, Namespace, URIRef
+from rdflib.plugins.stores.memory import Memory
+
+from grapho.namespace import PSDO
 
 
 def test_load_from_cp():
-    
     g = Graph()
     # g.bind("schema", "http://schema.org/", override=True, replace=True)
     assert len(g) == 0
-    
+
     g.parse("tests/social_better.json")
-    
-    ns = dict(g.namespaces())
-    
+
+    # ns = dict(g.namespaces())
+
     print(g.serialize(format="turtle"))
-    
-    assert len(g) == 21 
+
+    assert len(g) == 21
 
 
 def test_compact_graph():
-    
+    PSDO.foo
+    PSDO.bar
     g = Graph(bind_namespaces="core")
     g.bind("foaf", FOAF)
-    
+
     bob = URIRef("bob")
     alice = URIRef("alice")
     g.add((bob, RDF.type, FOAF.Person))
     g.add((bob, FOAF.name, Literal("Bob")))
     g.add((bob, FOAF.knows, alice))
     g.add((alice, FOAF.name, Literal("Alice")))
-    
-    
-    logger.info(g.serialize(format="json-ld",auto_compact=True))
-    
+
+    logger.info(g.serialize(format="json-ld", auto_compact=True))
+
     pass
+
 
 def test_compact_with_blank():
-    
     core_g = Graph(bind_namespaces="core")
-    core_g.bind("foaf",FOAF)
+    core_g.bind("foaf", FOAF)
 
     # Sadly, parse adds back in all the default "rdflib" namspaces
-    g = Graph().parse("./tests/bob_carol_ted_alice.json",format="json-ld",publicID="https://example.com/")
-    
+    g = Graph().parse(
+        "./tests/bob_carol_ted_alice.json",
+        format="json-ld",
+        publicID="https://example.com/",
+    )
+
     # so we set them back here
     g.namespace_manager.namespaces = core_g.namespaces
- 
-    logger.info(g.serialize(
-        format="json-ld",
-        auto_compact=True))
-    
+
+    logger.info(g.serialize(format="json-ld", auto_compact=True))
+
     pass
 
-        
-        
+
+def test_pocg():
+    """
+    An RDFLib ConjunctiveGraph is an (unnamed) aggregation of all the Named Graphs
+    within a Store. The :meth:`~rdflib.graph.ConjunctiveGraph.get_context`
+    method can be used to get a particular named graph for use, such as to add
+    triples to, or the default graph can be used.
+
+    This example shows how to create Named Graphs and work with the
+    conjunction (union) of all the graphs.
+    """
+
+    LOVE = Namespace("http://love.com#")
+    LOVERS = Namespace("http://love.com/lovers/")
+
+    mary = URIRef("http://love.com/lovers/mary")
+    john = URIRef("http://love.com/lovers/john")
+
+    cmary = URIRef("http://love.com/lovers/mary")
+    cjohn = URIRef("http://love.com/lovers/john")
+
+    store = Memory()
+
+    g = ConjunctiveGraph(store=store)
+    g.bind("love", LOVE)
+    g.bind("lovers", LOVERS)
+
+    # Add a graph containing Mary's facts to the Conjunctive Graph
+    gmary = Graph(store=store, identifier=cmary)
+    # Mary's graph only contains the URI of the person she loves, not his cute name
+    gmary.add((mary, LOVE.hasName, Literal("Mary")))
+    gmary.add((mary, LOVE.loves, john))
+
+    # Add a graph containing John's facts to the Conjunctive Graph
+    gjohn = Graph(store=store, identifier=cjohn)
+    # John's graph contains his cute name
+    gjohn.add((john, LOVE.hasCuteName, Literal("Johnny Boy")))
+
+    # Enumerate contexts
+    print("Contexts:")
+    for c in g.contexts():
+        print(f"-- {c.identifier} ")
+    print("===================")
+    # Separate graphs
+    print("John's Graph:")
+    print(gjohn.serialize())
+    print("===================")
+    print("Mary's Graph:")
+    print(gmary.serialize())
+    print("===================")
+
+    print("Full Graph")
+    print(g.serialize())
+    print("===================")
+
+    print("Query the conjunction of all graphs:")
+    xx = None
+    for x in g[mary : LOVE.loves / LOVE.hasCuteName]:  # type: ignore[misc]
+        xx = x
+    print("Q: Who does Mary love?")
+    print("A: Mary loves {}".format(xx))
+
+
+def test_alias():
+    # assert 'AKA: foo' == PSDO.alias('foo')
+    assert PSDO.PSDO_000123 == PSDO.foo
+    assert PSDO.PSDO_000123 == PSDO["foo"]
+
+    assert PSDO.PSDO_000124 == PSDO["PSDO_000124"]
+    assert PSDO.PSDO_000125 == PSDO.baz
+    assert "foo" in PSDO
+    assert "PSDO_000123" in PSDO
+
+    assert Namespace("http://purl.obolibrary.org/obo/") == PSDO._NS
+
+    # assert URIRef('http://purl.obolibrary.org/obo/foo') == PSDO._NS.foo
+
+    pass
